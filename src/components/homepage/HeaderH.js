d1d1604ecd746fd2e37c209a456501fae0a1a4c5
mirko-console-dir <cdmirko.saponaro@gmail.com>
2022-07-29 14:47:56 +1000

    js control image hero

--- src/components/homepage/HeaderH.js
+++ src/components/homepage/HeaderH.js
@@ -1,4 +1,39 @@
 import React from "react";
+
+// Cube
+let x = 0;
+let y = 20;
+let z = 0;
+
+const topXcontrol = () => {
+  let cube = document.querySelector(".cube");
+  cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+};
+
+const botXcontrol = () => {
+  let cube = document.querySelector(".cube");
+  cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+};
+
+const leftYcontrol = () => {
+  let cube = document.querySelector(".cube");
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg) `;
+};
+
+const rightYcontrol = () => {
+  let cube = document.querySelector(".cube");
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg) `;
+};
+
+const topZcontrol = () => {
+  let cube = document.querySelector(".cube");
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg) `;
+};
+
+const botZcontrol = () => {
+  let cube = document.querySelector(".cube");
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg) `;
+};
 const HeaderH = () => {
   return (
     <>
@@ -6,7 +41,7 @@ const HeaderH = () => {
       <div className="cube-wrapper">
         <div className="cube">
           <div className="front-side">
-            <img src="images/iphone.png" />
+            <img src="images/iphone.png" alt="image_product" />
           </div>
           <div className="back-side center">
             <i className="fab fa-apple" />
@@ -14,22 +49,34 @@ const HeaderH = () => {
         </div>
         {/* Controls for movement*/}
         <div className="controls">
-          <a href="#" className="top-x-control">
+          <a onClick={() => topXcontrol()} href="#" className="top-x-control">
             <i className="fas fa-arrow-up " />
           </a>
-          <a href="#" className="bottom-x-control">
+          <a
+            onClick={() => botXcontrol()}
+            href="#"
+            className="bottom-x-control"
+          >
             <i className="fas fa-arrow-down " />
           </a>
-          <a href="#" className="left-y-control">
+          <a onClick={() => leftYcontrol()} href="#" className="left-y-control">
             <i className="fas fa-arrow-left " />
           </a>
-          <a href="#" className="right-y-control">
+          <a
+            onClick={() => rightYcontrol()}
+            href="#"
+            className="right-y-control"
+          >
             <i className="fas fa-arrow-right " />
           </a>
-          <a href="#" className="top-z-control">
+          <a onClick={() => topZcontrol()} href="#" className="top-z-control">
             <i className="fas fa-arrow-down " />
           </a>
-          <a href="#" className="bottom-z-control">
+          <a
+            onClick={() => botZcontrol()}
+            href="#"
+            className="bottom-z-control"
+          >
             <i className="fas fa-arrow-up " />
           </a>
         </div>
